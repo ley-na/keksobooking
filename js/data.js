23bc53ba0daed9556b831137e90d3dfae8bb1af2
Elena Voytovich <elena-voytovich@yandex.ru>
2020-07-27 11:53:44 +0300

    Add a backend part

--- js/data.js
+++ js/data.js
@@ -1,16 +1,6 @@
 'use strict';
 
 (function () {
-  var OFFER_TITLES = ['Лучшее место для отдыха вдвоём', 'Квартира с видом на море', 'Студия в центре', 'Для большой семьи'];
-  var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var CHECK_INS = ['12:00', '13:00', '14:00'];
-  var CHECK_OUTS = ['12:00', '13:00', '14:00'];
-  var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var OFFER_DESCRIPTIONS = ['Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.', 'Второе описание', 'Третье описание'];
-  var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var OFFER_ROOMS = [1, 2, 3, 100];
-  var OFFER_GUESTS = [1, 2, 3, 0];
-  var PRICE_MAX = 400000;
 
   var OFFER_PHOTO_WIDTH = 45;
   var OFFER_PHOTO_HEIGHT = 40;
@@ -29,39 +19,6 @@
   };
 
   var locationXMax = document.querySelector('.map').offsetWidth;
-
-  // Генерирование элементов
-  var generateOffers = function (size) {
-    var offers = [];
-    for (var i = 1; i <= size; i++) {
-      var locationX = window.utils.getRandomNumber(LOCATION_X_MIN, locationXMax);
-      var locationY = window.utils.getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX);
-      var offer = {
-        author: {
-          avatar: 'img/avatars/user0' + i + '.png'
-        },
-        offer: {
-          title: window.utils.getRandomElement(OFFER_TITLES),
-          address: locationX + ', ' + locationY,
-          price: window.utils.getRandomNumber(0, PRICE_MAX),
-          type: window.utils.getRandomElement(OFFER_TYPES),
-          rooms: window.utils.getRandomElement(OFFER_ROOMS),
-          guests: window.utils.getRandomElement(OFFER_GUESTS),
-          checkin: window.utils.getRandomElement(CHECK_INS),
-          checkout: window.utils.getRandomElement(CHECK_OUTS),
-          features: window.utils.shuffleAndSliceItems(OFFER_FEATURES),
-          description: window.utils.getRandomElement(OFFER_DESCRIPTIONS),
-          photos: window.utils.shuffleAndSliceItems(OFFER_PHOTOS)
-        },
-        location: {
-          x: locationX,
-          y: locationY
-        }
-      };
-      offers.push(offer);
-    }
-    return offers;
-  };
 
   var renderFeatures = function (container, features) {
     container.innerHTML = '';
@@ -90,7 +47,6 @@
   };
 
   window.data = {
-    generateOffers: generateOffers,
     renderFeatures: renderFeatures,
     renderPhotos: renderPhotos,
     roomTypes: roomTypes,
--- js/map.js
+++ js/map.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (function () {
-  var OFFERS_NUMBER = 8;
+
+  var errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
 
   // Спрятать карту
   var disablePage = function () {
@@ -18,11 +19,11 @@
     window.pin.map.classList.remove('map--faded');
     window.form.ad.classList.remove('ad-form--disabled');
     window.form.setAddressEnabled();
-    window.pin.render(offers);
     window.form.enableElements(window.form.adFieldsets);
     window.form.enableElements(window.form.filterElements);
     document.removeEventListener('keydown', onPinPress);
     window.form.mainPin.removeEventListener('mousedown', onPinMousedown);
+    window.backend.load(onSuccessLoad, onErrorLoad);
   };
 
   var onPinPress = function (evt) {
@@ -37,7 +38,53 @@
     }
   };
 
-  var offers = window.data.generateOffers(OFFERS_NUMBER);
+  var onErrorEscPress = function (evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      closeError();
+    }
+  };
+
+  var onErrorClick = function (evt) {
+    if (evt.target === document.querySelector('div.error')) {
+      evt.preventDefault();
+      closeError();
+    }
+  };
+
+  var onErrorButtonClick = function () {
+    closeError();
+  };
+
+  var closeError = function () {
+    document.querySelector('div.error').remove();
+    document.removeEventListener('keydown', onErrorEscPress);
+    document.removeEventListener('keydown', onErrorClick);
+    document.removeEventListener('click', onErrorButtonClick);
+  };
+
+  var showError = function (errorMessage) {
+    var errorElement = errorMessageTemplate.cloneNode(true);
+
+    var messageText = errorElement.querySelector('.error__message');
+    messageText.textContent = errorMessage;
+
+    var message = document.querySelector('main').appendChild(errorMessageTemplate);
+    var errorButton = message.querySelector('.error__button');
+
+    document.addEventListener('keydown', onErrorEscPress);
+    document.addEventListener('click', onErrorClick);
+    errorButton.addEventListener('click', onErrorButtonClick);
+  };
+
+  var onSuccessLoad = function (data) {
+    window.pin.render(data);
+  };
+
+  var onErrorLoad = function (message) {
+    showError(message);
+  };
+
   disablePage();
   window.form.validateRooms();
   window.form.validatePrice();
